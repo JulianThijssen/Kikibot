e0577c77528256bba4472a29fdca17a3c8d54365
Julian Thijssen <Nimthora@gmail.com>
2017-07-05 15:45:35 +0200

    Add install script

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
+sudo apt-get update
 sudo apt-get install jsvc git
+cd /home
 mkdir kikibot
 cd kikibot
 git init
 git remote add origin https://github.com/JulianThijssen/Kikibot.git
 git pull origin master
-add db
-add jar
-move script to /etc/init.d/kikibot
+cp build/Kikibot.jar ./Kikibot.jar
+cp kikibot /etc/init.d/kikibot
 chmod 755 /etc/init.d/kikibot
-user file
-/etc/init.d/kikibot start
